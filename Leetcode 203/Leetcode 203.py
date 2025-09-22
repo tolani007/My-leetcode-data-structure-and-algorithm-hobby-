ecccea550e5229c1d3eba6f5570759cd7ab0556b
Tolani Akinola <34937584+tolani007@users.noreply.github.com>
2025-09-21 21:12:01 -0400

    Update Leetcode 203.py
    tweaked solution

--- Leetcode 203/Leetcode 203.py
+++ Leetcode 203/Leetcode 203.py
@@ -1,18 +1,15 @@
-class ListNode:
-  def __init__(self, val=0, next = None):
-    self.val = val
-    self.next = next
-
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution(object):
-  def removeElements(self, head, val):
-    dummy = ListNode(0)
-    dummy.next = head
-    current = dummy
-
-      while current.next:
-        if current.next.val ==val:
-          current.next = current.next.next
-        else:
-          current = current.next
-      return dummy.next
+    def removeElements(self, head, val):
+        dummy = ListNode(0, head)   # point dummy directly at head
+        current = dummy
+        while current.next:          # look ahead at the node we might delete
+            if current.next.val == val:
+                current.next = current.next.next   # skip the matching node
+            else:
+                current = current.next             # advance normally
+        return dummy.next
